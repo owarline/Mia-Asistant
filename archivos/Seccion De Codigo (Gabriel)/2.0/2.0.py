edd56b1247dc2185f15e6f8052f0cab965e99972
Owarline <103376782+owarline@users.noreply.github.com>
2022-04-21 09:37:24 -0400

    Update 2.0.py

--- archivos/Seccion De Codigo (Gabriel)/2.0/2.0.py
+++ archivos/Seccion De Codigo (Gabriel)/2.0/2.0.py
@@ -1,14 +1,29 @@
+#funciones totales del programa, dependencias
 import requerimientos as rq
+#que la maquina pueda reconocer dictados
 import speech_recognition as sr
+#pyttsx3: hablar
+#pywhatkit: llamdo de funciones ideal
+#wikipedia: sin necesidad de explicacion
+#datetime: hora y tiempo
+#keyboard: reconocer el teclado
+#os: buscar archivos
 import pyttsx3,pywhatkit,wikipedia,datetime,keyboard,os
+#pygame: videojuegos. mixer: subfuncion de musica
 from pygame import mixer
+#subprocess: ejecutar acciones en segundo plano
 import subprocess as sub
+#imprime en pantalla el titulo del asistente
 print(rq.imprimir_titulo())
+#nombre
 name="Mia"
+#funcion base
 engine = pyttsx3.init()
+#voces
 voices = engine.getProperty("voices")
+#que la funcion base pueda usar la primera voz (opcion 0)
 engine.setProperty("voice", voices[0].id)
-#funcion para que la maquina lea algo e voz alta
+#codigo fuente/troncal
 def run_chica():
     while True:
         try:
@@ -27,8 +42,46 @@ def run_chica():
             wiki = wikipedia.summary(search, 5)
             print(search + ":" + wiki)
             rq.talk(wiki)
+        elif "funciones" in rec:
+            rq.talk(
+                "mis funciones con necesidad de internet son: reproducir musica y buscar informacion en wikipedia, y abrir algunas paginas web.")
+            rq.talk(
+                "mis funciones sin necesidad de internet son: despedirme de forma educada y dominar a la humanidad, perdon, es decir, eso es todo"
+            )
+            print(
+                """
+            Online:
+            # Reproducir musica
+            # Buscar informacion
+            Offline:
+            # Ser mas educada que su creador
+            """)
+        elif "creditos" in rec:
+            rq.talk(
+                "El creador del codigo es Gabriel Parra, alias owarline, mis proyectistas son Consuelo Leiva, alias consueloleivau y Hugo Solís, alias hugosincum"
+            )
+            print(
+                "El creador del codigo es Gabriel Parra, alias owarline, mis proyectistas son Consuelo Leiva, alias consueloleivau y Hugo Solís, alias hugosincum"
+            )
+        elif "alarma" in rec:
+            num = rec.replace("alarma", "")
+            num = num.strip()
+            rq.talk("alarma activada a las " + num + " horas, dejaré de funcionar hasta la activación de la alarma")
+            print("alarma activada a las " + num + " horas")
+            while True:
+                if datetime.datetime.now().strftime("%H:%M") == num:
+                    print("ALARMA!!!")
+                    print("presiona 's' para detenerla")
+                    mixer.init()
+                    mixer.music.load("alarma.mp3")
+                    mixer.music.play()
+                    if keyboard.read_key() == "s":
+                        mixer.music.stop()
+                        break
         elif "gracias" in rec:
             rq.talk("De nada, adios")
             break
 if __name__ == "__main__":
     run_chica()
+
+#DERECHOS DE AUTOR: LICENCIA CREATIVE COMMONS, CITAR AUTORES PARA SU UTILIZACION.
